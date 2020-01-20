5bef251bc89fab89449e1d9b256596c14863cf82
Placido Wang <placidow@gmail.com>
2020-01-20 08:38:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  array_squared = []
+  
+  i = 0
+  while i < array.length do
+    array_squared << array[i] ** 2
+  end
+  
+  return array_squared
 end
